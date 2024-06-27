96d7034d7fb3f5d036382f5c1fe00597b7ee0158
kameamea133 <monnier1977@gmail.com>
2024-06-27 18:23:43 +0200

    react-picker added

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./chat.css"
+import EmojiPicker from 'emoji-picker-react';
 import { RxAvatar } from "react-icons/rx";
 import { FaPhone } from "react-icons/fa6";
 import { IoVideocam } from "react-icons/io5";
@@ -14,6 +15,16 @@ import { FaCamera, FaMicrophone } from "react-icons/fa";
 
 
 const Chat = () => {
+  const [open, setopen] = useState(false)
+  const [text, setText] = useState("")
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev +  e.emoji);
+    setopen(false)
+  }
+
+ 
+
   return (
     <div className='chat'>
       <div className='top'>
@@ -39,9 +50,12 @@ const Chat = () => {
         <FaCamera />
         <FaMicrophone />
         </div>
-        <input type="text" placeholder='type a message....'/>
+        <input type="text" placeholder='type a message....' onChange={(e) => setText(e.target.value)} value={text}/>
         <div className='emoji'>
-        <BsEmojiSmileFill />
+        <BsEmojiSmileFill  onClick={() => setopen((prev) => !prev)}/>
+        <div className='picker'>
+        <EmojiPicker open={open} onEmojiClick={handleEmoji}/>
+        </div>
         </div>
         <button className='sendButton'>Send</button>
       </div>
