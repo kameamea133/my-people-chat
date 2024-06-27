41970dc050aca67aa65805506529ef1b93410f06
kameamea133 <monnier1977@gmail.com>
2024-06-27 17:53:09 +0200

    input chat center added

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import "./chat.css"
+import { RxAvatar } from "react-icons/rx";
+import { FaPhone } from "react-icons/fa6";
+import { IoVideocam } from "react-icons/io5";
+import { IoIosInformationCircle } from "react-icons/io";
+import { BsEmojiSmileFill } from "react-icons/bs";
+import { FaImage } from "react-icons/fa6";
+import { FaCamera, FaMicrophone } from "react-icons/fa";
+
+
+
+
 
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className='top'>
+        <div className='user'>
+        <RxAvatar />
+
+        </div>
+        <div className='texts'>
+          <span>Jane Doe</span>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+        <div className='icons'>
+        <FaPhone size={20}/>
+        <IoVideocam size={20}/>
+        <IoIosInformationCircle size={20}/>
+
+        </div>
+      </div>
+      <div className='center'></div>
+      <div className='bottom'>
+        <div className='icons'>
+        <FaImage />
+        <FaCamera />
+        <FaMicrophone />
+        </div>
+        <input type="text" placeholder='type a message....'/>
+        <div className='emoji'>
+        <BsEmojiSmileFill />
+        </div>
+        <button className='sendButton'>Send</button>
+      </div>
+    </div>
   )
 }
 
